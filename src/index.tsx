2b2dd9a7364cd1499715c9566e2ee856da749aad
Suraj Thapa <stsurajst@gmail.com>
2022-04-07 12:22:14 +0545

    redux with ts setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import ReactDom from "react-dom";
-import { GuestList } from "./state/GuestList";
-import { UserSearch } from "./state/UserSearch";
 const App = () => {
   return (
     <div>
       <h1>Hi There!</h1>
-      <UserSearch />
     </div>
   );
 };
